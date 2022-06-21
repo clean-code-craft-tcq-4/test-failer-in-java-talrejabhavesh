5c7069598f3c7d1a879adbdf27b0dc2a32f739d0
talrejabhavesh <105204399+talrejabhavesh@users.noreply.github.com>
2022-06-21 23:07:37 +0530

    Update misaligned.java

--- misaligned/misaligned.java
+++ misaligned/misaligned.java
@@ -1,18 +1,38 @@
+package misaligned;
+
 public class Misaligned {
-    static int printColorMap() {
-        String majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
-        String minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-        int i = 0, j = 0;
-        for(i = 0; i < 5; i++) {
-            for(j = 0; j < 5; j++) {
-                System.out.printf("%d | %s | %s\n", i * 5 + j, majorColors[i], minorColors[i]);
-            }
+
+  public static int[] printColorMap() {
+    String majorColors[] = { "White", "Red", "Black", "Yellow", "Violet" };
+    String minorColors[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
+    int i = 0, j = 0, firstSeparator = 0, lastSeparator = 0, numberOfMisaligned = 0;
+    // to iterate all combination of colors
+    for (i = 0; i < 5; i++) {
+      for (j = 0; j < 5; j++) {
+        String eachline = (i * 5 + j) + " | " + majorColors[i] + " | " + minorColors[i];
+        System.out.println(eachline);
+        // to get the position of separator in first line
+        if (i == 0 && j == 0) {
+          firstSeparator = eachline.indexOf('|');
+          lastSeparator = eachline.lastIndexOf('|');
         }
-        return i * j;
+        // to check whether the next line is aligned with previous line
+        boolean isAligned = (firstSeparator == eachline.indexOf('|') && lastSeparator == eachline.lastIndexOf('|'));
+        if (!isAligned) {
+          numberOfMisaligned++;
+        }
+      }
     }
-    public static void main(String[] args) { 
-        int result = printColorMap();
-        assert(result == 25);
-        System.out.println("All is well (maybe!)");
-    }
+    //result array will contain total no of combination printed and how many lines are misaligned
+    int[] resultArray = { (i * j), numberOfMisaligned };
+    return resultArray;
+  }
+
+
+  public static void main(String[] args) {
+    MisalignedTest test=new MisalignedTest();
+    test.AlignmentTest();
+  }
+
+
 }
