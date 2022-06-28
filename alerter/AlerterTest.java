bbc2f58536283c9ad79d9987ba836f12460aa4d9
talrejabhavesh <105204399+talrejabhavesh@users.noreply.github.com>
2022-06-29 00:52:13 +0530

    Update AlerterTest.java

--- alerter/AlerterTest.java
+++ alerter/AlerterTest.java
@@ -4,9 +4,9 @@ public class AlerterTest {
 
   
   public void alertNetworkTest() {
-    Alerter.alertInCelcius(400.5f);
-    assert(Alerter.alertFailureCount==1);
-    Alerter.alertInCelcius(503.6f);
+    alerter.alertInCelcius(400.5f);
+    assert(alerter.alertFailureCount==1);
+    alerter.alertInCelcius(503.6f);
     assert(Alerter.alertFailureCount==2);
   }
 }
