b75ec0670c807336b696e3dc47dd6e4a8d6522a1
talrejabhavesh <105204399+talrejabhavesh@users.noreply.github.com>
2022-06-29 00:29:10 +0530

    Update MisalignedTest.java

--- misaligned/MisalignedTest.java
+++ misaligned/MisalignedTest.java
@@ -3,10 +3,15 @@ package misaligned;
 public class MisalignedTest {
 
   
+
+  
   public void AlignmentTest() {
-    int[] result = Misaligned.printColorMap();
-    assert (result[0] == 25);
-    assert (result[1] == 0);
-    System.out.println("All is well (maybe!)");
+    int result = Misaligned.printColorMap();
+    assert (result == 25);
+    Misaligned.createReferenceManualMap();
+    List<String> colorPair=Misaligned.getColorPair(3);
+    assert(colorPair.get(0)=="White");
+    assert(colorPair.get(0)=="Green");
+    System.out.println("All is well");
   }
 }
