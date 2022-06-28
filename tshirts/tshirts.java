c01975b28ee927b1935d9f09a783b88377f3604e
talrejabhavesh <105204399+talrejabhavesh@users.noreply.github.com>
2022-06-28 23:23:43 +0530

    Update tshirts.java

--- tshirts/tshirts.java
+++ tshirts/tshirts.java
@@ -2,13 +2,17 @@ package tshirts;
 
 public class Tshirts {
  public static String size(int cms) {
-      if(cms < 38) {
+      if(cms>=32 && cms < 38) {
           return "S";
-      } else if(cms > 38 && cms < 42) {
+      } else if(cms >= 38 && cms < 42) {
           return "M";
-      } else {
+      } else if(cms >= 42 && cms < 48){
           return "L";
+      }else {
+        return "Entered number is not a valid size";
       }
+  }
+
   }
 
   public static void main(String[] args) { 
